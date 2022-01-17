6704e3269fff047ce17d5d42b6d0663820d6b878
Marcelo Barbosa <marcelo.barbosa@coopersytem.com.br>
2022-01-17 11:57:22 -0300

    refactor: chamando telas pelas rotas no app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
+// import AppProvider from './src/contexts'
+import { Routes } from './routes';
+// import {
+//   useFonts,
+//   Roboto_400Regular,
+//   Roboto_500Medium,
+//   Roboto_700Bold
+// } from '@expo-google-fonts/roboto';
+
+// import theme from './src/styles/theme';
 
 export default function App() {
+  // const [fontsLoaded] = useFonts({
+  //   Roboto_400Regular,
+  //   Roboto_500Medium,
+  //   Roboto_700Bold
+  // });
+
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // }
   return (
-    <View style={styles.container}>
-      <Text>QUIZ</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <ThemeProvider theme={theme}> */}
+      {/* <AppProvider> */}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#121214' }}>
+        <Routes />
+      </SafeAreaView>
+      {/* </AppProvider> */}
+      {/* </ThemeProvider> */}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
