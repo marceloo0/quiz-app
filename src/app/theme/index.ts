60e4fb1dc64add4fc19e0c5992178ec2f8e64408
Marcelo Barbosa <marcelo.barbosa@coopersytem.com.br>
2022-01-17 12:16:53 -0300

    feat: adicionando cores do app

---
+++ src/app/theme/index.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    background_primary: '#F5F5FA',
+    background_secondary: '#FFFFFF',
+    background_tertiary: '#E1F5EC',
+
+    title: '#514766',
+    text: '#6E6680',
+    text_secondary: '#A6A1B2',
+    text_placeholder: '#C9C7C7',
+    text_tertiary: '#03AB4F',
+
+    button: '#04D361',
+    button_secondary: '#E83F5B',
+    button_tertiary: '#8257E5',
+
+    line: '#D9D7DA',
+
+    border: '#F5F5F5',
+    border_card: '#E1E1E5',
+  },
+
+  fonts: {
+    primary_400: 'Roboto_400Regular',
+    primary_500: 'Roboto_500Medium',
+    primary_700: 'Roboto_700Bold',
+  },
+};
