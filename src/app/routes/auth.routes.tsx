046ac043548c203943af6a2e26b1d5d1d035c484
Marcelo Barbosa <marcelo.barbosa@coopersytem.com.br>
2022-01-17 11:47:45 -0300

    chore: atulizando caminho de telas

--- src/app/routes/auth.routes.tsx
+++ src/app/routes/auth.routes.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { useTheme } from 'styled-components';
 
-import { HomeScreen } from '../screens/HomeScreen';
-import { CongratulationScreen } from '../screens/CongratulationScreen';
-import { QuestionsScreen } from '../screens/QuestionsScreen';
-import { ResultScreen } from '../screens/ResultScreen';
+import { HomeScreen } from '../../screens/HomeScreen';
+import { CongratulationScreen } from '../../screens/CongratulationScreen';
+import { QuestionsScreen } from '../../screens/QuestionsScreen';
+import { ResultScreen } from '../../screens/ResultScreen';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
