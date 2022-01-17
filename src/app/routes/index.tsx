03c5aff70e28aa0c35aa15b47cf8a6f7c8373ef5
Marcelo Barbosa <marcelo.barbosa@coopersytem.com.br>
2022-01-17 11:58:16 -0300

    refactor: buscando nome correto

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 
 // import { useAuth } from '../hooks/useAuth';
-// import { AppTabRoutes } from './app.tab.routes';
-import { AppStackRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
+// import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   // const { username, loading } = useAuth();
@@ -16,6 +16,6 @@ export function Routes() {
   //     </View>
   //   );
   // }
-  // return !username ? <AppStackRoutes /> : <AppTabRoutes />;
-  return <AppStackRoutes />;
+  // return !username ? <AuthRoutes /> : <AppTabRoutes />;
+  return <AppRoutes />;
 }
